237fbada9581d02eec06f15ed2eae3f3ca095a83
thandi mpofu <thandimpofu2003@gmail.com>
2023-08-09 20:26:06 -0500

    deploy15

--- backend/db/migrations/20230809184719-create-community-members.js
+++ backend/db/migrations/20230809184719-create-community-members.js
@@ -40,7 +40,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    }), options;
+    }, options);
   },
   async down (queryInterface, Sequelize) {
     options.tableName = "CommunityMembers";
